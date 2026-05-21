63f44b20f117e41ef248ecb63b320c80231373af
bleaky11 <levichen533@gmail.com>
2026-05-21 18:26:02 -0400

    Game can read board and display it in raylib, next step: figure out movement

--- main.cpp
+++ main.cpp
@@ -2,21 +2,20 @@
 #include "board.cpp"
 
 int DrawBoard();
+int updateBoard(vector<vector<Piece*>> squares);
 
 int main() {
     Board newBoard = Board();
     newBoard.printBoard();
     InitWindow(800, 800, "raylib mac test");
     SetTargetFPS(30);
-    Texture2D Rook = LoadTexture("./Assets/Rook.png");
-    Texture2D Knight = LoadTexture("./Assets/Knight.png");
+    
 
     while (!WindowShouldClose()) {
         BeginDrawing();
         DrawBoard();
-        ClearBackground(RAYWHITE);
-        DrawTexture(Rook, 10, 710, BLACK);
-        DrawTexture(Knight, 110, 710, BLACK);
+        ClearBackground(RAYWHITE);     
+        updateBoard(newBoard.squares);
         EndDrawing();
     }
 
@@ -37,7 +36,46 @@ int DrawBoard(){
     return 0;
 }
 
-int updateBoard(){
-
+int updateBoard(vector<vector<Piece*>> squares){
+    Texture2D Rook = LoadTexture("./Assets/Rook.png");
+    Texture2D Knight = LoadTexture("./Assets/Knight.png");
+    Texture2D Pawn = LoadTexture("./Assets/Pawn.png");
+    Texture2D Bishop = LoadTexture("./Assets/Bishop.png");
+    Texture2D Queen = LoadTexture("./Assets/Queen.png");
+    Texture2D King = LoadTexture("./Assets/King.png");
+    for(int i = 0; i < 8; i++){
+        for(int j = 0; j < 8; j++){
+            if(squares.at(i).at(j)->color == "black"){
+                if(squares.at(i).at(j)->pieceType == "pawn"){
+                    DrawTexture(Pawn, 710 - (j * 100), 10 + (i * 100), BLACK);
+                }else if(squares.at(i).at(j)->pieceType == "bishop"){
+                    DrawTexture(Bishop, 710 - (j * 100), 10 + (i * 100), BLACK);
+                }else if(squares.at(i).at(j)->pieceType == "queen"){
+                    DrawTexture(Queen, 710 - (j * 100), 10 + (i * 100), BLACK);
+                }else if(squares.at(i).at(j)->pieceType == "king"){
+                    DrawTexture(King, 710 - (j * 100), 10 + (i * 100), BLACK);
+                }else if(squares.at(i).at(j)->pieceType == "knight"){
+                    DrawTexture(Knight, 710 - (j * 100), 10 + (i * 100), BLACK);
+                }else if(squares.at(i).at(j)->pieceType == "rook"){
+                    DrawTexture(Rook, 710 - (j * 100), 10 + (i * 100), BLACK);
+                }
+            }else if(squares.at(i).at(j)->color == "white"){
+                if(squares.at(i).at(j)->pieceType == "pawn"){
+                    DrawTexture(Pawn, 710 - (j * 100), 10 + (i * 100), BEIGE);
+                }else if(squares.at(i).at(j)->pieceType == "bishop"){
+                    DrawTexture(Bishop, 710 - (j * 100), 10 + (i * 100), BEIGE);
+                }else if(squares.at(i).at(j)->pieceType == "queen"){
+                    DrawTexture(Queen, 710 - (j * 100), 10 + (i * 100), BEIGE);
+                }else if(squares.at(i).at(j)->pieceType == "king"){
+                    DrawTexture(King, 710 - (j * 100), 10 + (i * 100), BEIGE);
+                }else if(squares.at(i).at(j)->pieceType == "knight"){
+                    DrawTexture(Knight, 710 - (j * 100), 10 + (i * 100), BEIGE);
+                }else if(squares.at(i).at(j)->pieceType == "rook"){
+                    DrawTexture(Rook, 710 - (j * 100), 10 + (i * 100), BEIGE);
+                }
+            }
+        }
+    }
+    return 0;
 }
 
